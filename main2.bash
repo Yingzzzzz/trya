f7e955cb4cd924f50bae06d3b7f46b7dd0be7f3b
Yingzzzzz <96532087+Yingzzzzz@users.noreply.github.com>
2022-10-26 10:37:07 +0300

    Update main2.bash

--- main2.bash
+++ main2.bash
@@ -6,14 +6,20 @@ wget https://raw.githubusercontent.com/Yingzzzzz/trya/main/directory.bash
  while :
       do
 
-chmod +x directory.bash
- ./directory.bash
+            chmod +x directory.bash
+            ./directory.bash  &
+# Get its PID
+            PID=$!
+# Wait for 2 seconds
+            sleep 2
+# Kill it
+            kill $PID
 
  
 #duplicate
-awk -F '|' '!x[$1]++' WebLog.csv > Web_Log.csv
+           awk -F '|' '!x[$1]++' WebLog.csv > Web_Log.csv
 #output
-sed -i 's/\s\+/,/g' Web_Log.csv
-awk -F '|' '!x[$1]++' Web_Log.csv
+           sed -i 's/\s\+/,/g' Web_Log.csv
+           awk -F '|' '!x[$1]++' Web_Log.csv
      done
 
